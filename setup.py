b7cba39f97635871eb01bdef93194f254dca507f
xaoyo <xaoyaoo@qq.com>
2023-10-15 10:46:26 +0800

    将整个项目作为包安装，增加命令行统一操作

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-version = "2.0.8"
+version = "2.1.0"
 setup(
     name="pywxdump",
     author="xaoyaoo",
