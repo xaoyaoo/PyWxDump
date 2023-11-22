05ab2e26fa321340bc8cb91697d605672a2e5dd7
xaoyo <xaoyaoo@qq.com>
2023-11-22 18:40:37 +0800

    添加all命令中解密错误数据日志写入文件,修复部分bug #28 #29

--- pywxdump/command.py
+++ pywxdump/command.py
@@ -374,7 +374,8 @@ def console_run():
     # 检查是否需要显示帮助信息
     if len(sys.argv) == 1:
         sys.argv.append('-h')
-    elif len(sys.argv) == 2 and sys.argv[1] in modes.keys():
+    elif len(sys.argv) == 2 and sys.argv[1] in modes.keys() and sys.argv[1] not in [main_all.mode, main_wx_info.mode,
+                                                                                    main_wx_db_path.mode]:
         sys.argv.append('-h')
 
     args = parser.parse_args()  # 解析命令行参数
