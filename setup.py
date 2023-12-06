fab27cc9221be959414e7e738d79bfd5b2c5cffe
xaoyaoo <xaoyaoo@qq.com>
2023-12-06 12:01:56 +0800

    更新v2.2.18

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-version = "2.2.17"
+version = "2.3.0"
 
 install_requires = [
     "psutil",
