de580a35f71026b6b9d63b822d520aaff9100b38
xaoyaoo <xaoyaoo@qq.com>
2023-12-09 22:52:50 +0800

    merge命令错误修复

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-version = "2.3.6"
+version = "2.3.5"
 
 install_requires = [
     "psutil",
