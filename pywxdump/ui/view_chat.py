2cd03e50784f999ac0db009f008894a6c25ad738
xaoyaoo <xaoyaoo@qq.com>
2023-12-21 21:00:05 +0800

    设置FileStorage_path为非必须选项

--- pywxdump/ui/view_chat.py
+++ pywxdump/ui/view_chat.py
@@ -176,12 +176,15 @@ def load_chat_records(selected_talker, start_index, page_size, user_list, MSG_AL
             match = re.search(r"MsgAttach(.*?)'", BytesExtra)
             if match:
                 img_path = match.group(0).replace("'", "")
-                print(FileStorage_path)
-                print(img_path)
-                img_path = os.path.join(FileStorage_path, img_path)
-                fomt, md5, out_bytes = read_img_dat(img_path)
-                out_bytes = base64.b64encode(out_bytes).decode("utf-8")
-                content["src"] = f"data:{fomt};base64,{out_bytes}"
+                # print(FileStorage_path)
+                # print(img_path)
+                if FileStorage_path:
+                    img_path = os.path.join(FileStorage_path, img_path)
+                    fomt, md5, out_bytes = read_img_dat(img_path)
+                    out_bytes = base64.b64encode(out_bytes).decode("utf-8")
+                    content["src"] = f"data:{fomt};base64,{out_bytes}"
+                else:
+                    content["src"] = ""
             else:
                 content["src"] = ""
             content["msg"] = "图片"
