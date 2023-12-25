30e64d5ffde3a8f48cda788344afc7cb8e1c3f9a
xaoyaoo <xaoyaoo@qq.com>
2023-12-25 16:30:00 +0800

    更新测试

--- tests/test_read_info.py
+++ tests/test_read_info.py
@@ -5,9 +5,10 @@
 # Author:       xaoyaoo
 # Date:         2023/10/21
 # -------------------------------------------------------------------------------
-
 from pywxdump.wx_info import read_info
 
 from pywxdump import VERSION_LIST_PATH, VERSION_LIST
 
-result = read_info(VERSION_LIST, is_logging=True)  # 读取微信信息
+def test_read_info():
+    result = read_info(VERSION_LIST, is_logging=True)  # 读取微信信息
+    assert result is not None
