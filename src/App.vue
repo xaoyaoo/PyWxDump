054ade4b293dedaf0f92c9a8675148b962231e51
xaoyaoo <xaoyaoo@qq.com>
2024-01-02 11:36:49 +0800

    Squashed 'pywxdump/ui/web/' content from commit 7283129
    git-subtree-dir: pywxdump/ui/web
    git-subtree-split: 7283129becd0b0027b801c312fd904a202422f73

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
