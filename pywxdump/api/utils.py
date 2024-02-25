1f7164fe8586dbba02ec3acd1c5978839033b773
xaoyaoo <xaoyaoo@qq.com>
2024-02-26 00:48:47 +0800

    html fix

--- pywxdump/api/utils.py
+++ pywxdump/api/utils.py
@@ -5,8 +5,10 @@
 # Author:       xaoyaoo
 # Date:         2024/01/16
 # -------------------------------------------------------------------------------
+import base64
 import json
 import logging
+import os
 import traceback
 from .rjson import ReJson
 from functools import wraps
@@ -41,3 +43,22 @@ def error9999(func):
             return ReJson(9999, body=rdata)
 
     return wrapper
+def gen_base64(path):
+    # 获取文件名后缀
+    extension = os.path.splitext(path)[1]
+    if extension == '.js':
+        start_str = 'data:text/javascript;base64,'
+    elif extension == '.css':
+        start_str = 'data:text/css;base64,'
+    elif extension == '.html':
+        start_str = 'data:text/html;base64,'
+    elif extension == '.json':
+        start_str = 'data:application/json;base64,'
+    else:
+        start_str = 'data:text/plain;base64,'
+
+    with open(path, 'rb') as file:
+        js_code = file.read()
+
+    base64_encoded_js = base64.b64encode(js_code).decode('utf-8')
+    return start_str + base64_encoded_js
