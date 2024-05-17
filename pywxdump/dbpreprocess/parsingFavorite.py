4816102c016bb9bfe44df51f4dda932f34f35dc1
xaoyaoo <xaoyaoo@qq.com>
2024-05-18 00:26:53 +0800

    准备添加收藏数据库解析

--- pywxdump/dbpreprocess/parsingFavorite.py
+++ pywxdump/dbpreprocess/parsingFavorite.py
@@ -7,6 +7,11 @@
 # -------------------------------------------------------------------------------
 from .dbbase import DatabaseBase
 
+# * FavItems：收藏的消息条目列表
+# * FavDataItem：收藏的具体数据。大概可以确定以下两点
+#     * 即使只是简单收藏一篇公众号文章也会在 FavDataItem 中有一个对应的记录
+#     * 对于收藏的合并转发类型的消息，合并转发中的每一条消息在 FavDataItem 中都是一个独立的记录
+# * FavTags：为收藏内容添加的标签
 
 class ParsingFavorite(DatabaseBase):
     _class_name = "Favorite"
@@ -15,6 +20,10 @@ class ParsingFavorite(DatabaseBase):
         super().__init__(db_path)
 
     def get_favorite(self):
-        sql = "select * from FavItems"
-        DBdata = self.execute_sql(sql)
-        return DBdata
+        sql1 = "select * from FavItems"
+        sql2 = "select * from FavDataItem"
+        sql3 = "select * from FavTags"
+        DBdata1 = self.execute_sql(sql1)
+        DBdata2 = self.execute_sql(sql2)
+        DBdata3 = self.execute_sql(sql3)
+        return DBdata1, DBdata2, DBdata3
