33da8c4b78e13ad375cf3fc273c430549c633956
xaoyaoo <xaoyaoo@163.com>
2024-08-17 12:25:13 +0800

    该flask为fastapi，速度更快

--- pywxdump/db/utils/__init__.py
+++ pywxdump/db/utils/__init__.py
@@ -8,3 +8,6 @@
 from ._loger import db_loger
 from .common_utils import timestamp2str, xml2dict, silk2audio, bytes2str, get_md5, name2typeid, typeid2name, \
     type_converter, match_BytesExtra, db_error, download_file, dat2img
+
+__all__ = ["db_loger", "timestamp2str", "xml2dict", "silk2audio", "bytes2str", "get_md5", "name2typeid", "typeid2name",
+           "type_converter", "match_BytesExtra", "db_error", "download_file", "dat2img"]
